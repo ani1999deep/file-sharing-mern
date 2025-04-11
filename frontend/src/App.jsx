469261a0c7eb837a02195c0b1bf90963b7cc5a2f
Anideep1 <anideepbhowmick1@gmail.com>
2025-04-11 17:10:35 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>File Sharing App</h1>
+
+      <div>
+        <button>Upload</button>
+        <input type="file" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
