becc933cf1347f30de56b5b83056051429975fd5
Anideep1 <anideepbhowmick1@gmail.com>
2025-04-11 17:27:33 +0530

    upload file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const uploadRef = useRef();
+
+  const handleUpload = () => {
+    uploadRef.current.click();
+  };
+  console.log("====>", file);
   return (
     <div className="container">
       <h1>File Sharing App</h1>
 
       <div>
-        <button>Upload</button>
-        <input type="file" />
+        <button
+          onClick={() => {
+            handleUpload();
+          }}
+        >
+          Upload
+        </button>
+        <input
+          type="file"
+          ref={uploadRef}
+          style={{ display: "none" }}
+          onChange={(event) => setFile(event.target.files[0])}
+        />
       </div>
     </div>
   );
